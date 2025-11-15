601a564b8338d370f52f363e88d5ebadd68ab797
Eric Laquer <LaquerEric@gmail.com>
2025-11-14 20:19:31 -0500

    removed old ex

--- examples/parent_module/submodules/ex/child/lib/parent.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'active_record'
-require 'active_support'
-require 'active_data_flow'
-
-require_relative 'active_data_flow/active_record/version'
-require_relative 'active_data_flow/sink/active_record'
-require_relative 'active_data_flow/source/active_record'
--- examples/parent_module/submodules/ex/child/lib/parent/active_record/version.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module ActiveDataFlow
-  module ActiveRecord
-    VERSION = '0.1.1'
-  end
-end
